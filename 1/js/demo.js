36e91959e1ae1a32d0fb02228218e8e920e3e1f9
Mar Prieto N <mar.pnieto@gmail.com>
2022-03-23 20:36:52 +0100

    Introduccion

---
+++ 1/js/demo.js
@@ -0,0 +1,5 @@
+"use strict";
+
+document.getElementById("name").value = "Mi nombre";
+document.getElementById("lastName").value = "Mi apellido";
+document.getElementById("avatar").src = "img/avatar_female.svg";
