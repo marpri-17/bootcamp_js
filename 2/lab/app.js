5cd2d0f177340073e7b0ecb36f6994261bdf0594
Mar Prieto N <mar.pnieto@gmail.com>
2022-03-28 22:03:48 +0200

    added stars ranking from solution

--- 2/lab/app.js
+++ 2/lab/app.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// Hotels
 var availableHotels = {
     Barcelo: {
         name: 'Hotel Barceló',
@@ -10,16 +10,38 @@ var availableHotels = {
         name: 'Hotel Ritz',
         location: 'Madrid',
         img: 'https://st3.idealista.com/news/archivos/styles/news_detail/public/2020-01/ritz_principal.jpg?sv=lY_5b6GC&itok=rg0OlBfF'
+    },
+    'NH Aranjuez': {
+        name: 'NH Collection Palacio de Aranjuez',
+        location: 'Aranjuez, Madrid',
+        img: 'https://media-cdn.tripadvisor.com/media/photo-s/21/ca/1b/e1/nh-collection-palacio.jpg'
     }
 }
+
+// Stars ranking template
+var starRanking = {
+    1:
+    "<span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
+    2:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
+    3:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span>",
+    4:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9734;</span>",
+    5:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>",
+}
+
+
 // get review display user preferences
 var isAnonimousReview = confirm('¿Quiere que su reseña sea anónima?');
 var userReview = prompt('Puntue este hotel de 1 a 5 estrellas');
-var selectedHotel = prompt("¿Qué hotel desea puntuar: 'Barcelo' o 'Ritz'?");
+var selectedHotel = prompt("¿Qué hotel desea puntuar: 'Barcelo', 'Ritz' o 'NH Aranjuez'?");
 
 
 // DOM
 var anonimousReviewCheck = document.getElementById('anonimous-review-check');
+
 var rankingElement = document.getElementById('stars-ranking')
     // Hotel desc
 var nameHotelContainer = document.getElementById('hotel-name');
@@ -33,4 +55,4 @@ locationHotelContainer.innerHTML = "Ubicado en " + availableHotels[selectedHotel
 imageHotelElement.src = availableHotels[selectedHotel].img;
 
 anonimousReviewCheck.checked = isAnonimousReview;
-rankingElement.innerHTML = userReview + ''; // Add space between star and number
+rankingElement.innerHTML = starRanking[userReview]; // Add space between star and number
