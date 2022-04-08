5699a0a4566728b373b34c6ca83c9aae580e05b7
Mar Prieto N <mar.pnieto@gmail.com>
2022-04-08 17:27:47 +0200

    mod. 4 calculadora

---
+++ 4/calculadora/app.js
@@ -0,0 +1,101 @@
+"use strict";
+
+/**
+ * Calculadora secuencial
+ */
+
+ const operation = {
+    type: null,
+    operator1: 0,
+    operator2: 0,
+}
+
+// Results
+let operationResult = 0;
+
+// DOM Elements
+const number1 = document.getElementById('number1');
+const number2 = document.getElementById('number2');
+const resultContainer = document.getElementById('resultContainer');
+const resultElement = document.getElementById('result');
+const resultRestElement = document.getElementById('rest');
+const resultErrorElement = document.getElementById('resultError');
+
+// Core Functions
+const sum = (a, b) => a + b;
+const rest = (a, b) => a - b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => a/b;
+
+const doOperation = (operationType) => {
+    switch (operationType) {
+        case 'sum':
+            operationResult = sum(operation.operator1, operation.operator2);
+            break;
+        case 'minus':
+            operationResult = rest(operation.operator1, operation.operator2);
+            break;
+        case 'multiply':
+            operationResult = multiply(operation.operator1, operation.operator2);
+            break;
+        case 'divide':
+            operationResult = divide(operation.operator1, operation.operator2);            
+            break;
+        default:
+            break;
+    }
+}
+
+function onClickOperation(operationType) {
+    // Always hide error at click
+    if (!resultError.classList.contains('hide')) {
+        toggleHidden(resultError);
+    }
+    // Always display result
+    if (resultContainer.classList.contains('hide')) {
+        toggleHidden(resultContainer)
+    }
+    // Check input values is number
+    let firstNumber = isNaN(number1.value) ? null : +number1.value;
+    let secondNumber = isNaN(number2.value) ? null : +number2.value;
+    if (firstNumber && secondNumber) {
+        operation.operator1 = firstNumber;
+        operation.operator2 = secondNumber;
+        doOperation(operationType);
+        printResults();
+    } else {
+        printError()
+    }
+}
+
+function printResults() {
+    resultElement.innerHTML = operationResult;
+}
+
+const toggleHidden = (domElement) => domElement.classList.toggle('hide');
+
+function printError() {
+    resultErrorElement.innerHTML = 'Sólo se admiten valores númericos';
+    toggleHidden(resultErrorElement);
+    toggleHidden(resultContainer);
+
+}
+
+
+function getOperationBySign(sign){
+    switch (sign) {
+        case '+':
+            return 'sum'
+        case '-':
+            return 'minus'
+        case 'x':
+        case 'X':
+        case '*':
+            return 'multiply'
+        case '/':
+        case '\\':
+            return 'divide'
+        default:
+            return 'ERROR';
+    }
+}
